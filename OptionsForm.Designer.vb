1daadd7eda73e7fddf01b4d4376408f9c7353642
MineUK Dev Team <admin@mineuk.com>
2013-01-16 23:06:49 +0000

    Updated the options form

--- OptionsForm.Designer.vb
+++ OptionsForm.Designer.vb
@@ -37,12 +37,12 @@ Partial Class OptionsForm
         '
         'Label1
         '
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(12, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(260, 20)
+        Me.Label1.Size = New System.Drawing.Size(260, 22)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "How much RAM do you want to allocate?"
+        Me.Label1.Text = "How Much RAM Do You Need?"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'RadioButton1
